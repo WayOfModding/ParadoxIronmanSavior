87766cecb4ca90e64492fed1ab5a2fa6ca5fc2c3
KaiserKatze <donizyo@gmail.com>
2018-10-23 18:56:35 +0800

    Bugfix 812e654

--- ParadoxSaveUtils/BackupPool.cs
+++ ParadoxSaveUtils/BackupPool.cs
@@ -48,71 +48,99 @@ namespace ParadoxSaveUtils
                     @"Function `BackupPool:add(saveFile={0})` invoked ... ",
                     saveFile));
 
+            SaveFile challenge;
+            string sPathSave, sPathChallenge;
+            DateTime time;
             int iVersion = saveFile.Version;
-            DateTime time = saveFile.LastWriteTimeUtc;
-            // avoid duplicate entries
-            bool result1 = false;
-            bool result2 = false;
-            bool result3 = false;
+            bool result = false;
 
-            if (listSaves.ContainsKey(time))
+            // Add an existing file into the pool:
+            if (System.IO.File.Exists(saveFile.AbsolutePath))
             {
-                SaveFile conflict = listSaves[time];
-                using (var fs1 = System.IO.File.OpenRead(saveFile.AbsolutePath))
-                using (var fs2 = System.IO.File.OpenRead(conflict.AbsolutePath))
+                sPathSave = saveFile.AbsolutePath;
+                time = saveFile.LastWriteTimeUtc;
+                challenge = null;
+                if (listSaves.ContainsKey(time))
+                {
+                    challenge = listSaves[time];
+                }
+                else
+                {
+                    challenge = this.getNewest();
+                }
+            }
+            // Allocate disk space for a new file
+            else
+            {
+                sPathSave = this.FileSave;
+                time = System.IO.File.GetLastWriteTimeUtc(sPathSave);
+                challenge = this.getNewest();
+            }
+
+            // Challenge got
+            // compare it with the items already in the pool
+            if (challenge != null)
+            {
+                sPathChallenge = challenge.AbsolutePath;
+
+                // compare MD5 hash of two files
+                using (var fs1 = System.IO.File.OpenRead(sPathSave))
+                using (var fs2 = System.IO.File.OpenRead(sPathChallenge))
                 {
                     var hash1 = MD5.Create().ComputeHash(fs1);
                     var hash2 = MD5.Create().ComputeHash(fs2);
-                    result3 = !hash1.SequenceEqual(hash2);
+
+                    result = !hash1.SequenceEqual(hash2);
 #if DEBUG
                     string sHash1 = BitConverter.ToString(hash1).Replace("-", "").ToLowerInvariant();
                     string sHash2 = BitConverter.ToString(hash2).Replace("-", "").ToLowerInvariant();
                     System.Diagnostics.Debug.Write(String.Format(
-                        @"Comparing conflict files: file/a({0}) - file/b({1}) ... ",
+                        @"Comparing conflict files: file/ver[{0}]({2}) - file/ver[{1}]({3}) ... ",
+                        iVersion,
+                        challenge.Version,
                         sHash1.Substring(0, 7),
                         sHash2.Substring(0, 7)));
 #endif
                 }
-                result1 = false;
+
+                bool containsVersion = dictSaves.ContainsKey(iVersion);
+                if (containsVersion)
+                {
+                    System.Diagnostics.Debug.Write(String.Format(
+                        @"Dictionary `dictSaves` contains key: (version={0}) ... ",
+                        iVersion));
+                }
+                result &= !containsVersion;
             }
+            // No challenge, no comparison, auto-success
             else
             {
-                listSaves[time] = saveFile;
-                result1 = true;
-            }
-            if (!dictSaves.ContainsKey(iVersion))
-            {
-                dictSaves[iVersion] = saveFile;
-                result2 = true;
+                result = true;
             }
 
-            bool result = (result1 || result3) && result2;
-            if (!result)
+            if (result)
             {
-                if (result1)
-                    listSaves.Remove(time);
-                if (result2)
-                    dictSaves.Remove(iVersion);
-#if !DEBUG
+                // on success
+                listSaves[time] = saveFile;
+                dictSaves[iVersion] = saveFile;
+            }
+#if ! DEBUG
+            else
+            {
                 // remove duplicate save file
                 string sFilePath = saveFile.AbsolutePath;
                 if (System.IO.File.Exists(sFilePath))
                     System.IO.File.Delete(sFilePath);
-#endif
             }
+#endif
 
             System.Diagnostics.Debug.WriteLine(
-                String.Format(
-                    @"{0}{1}",
-                    result ? "Success" : "Failure",
-                    result ? "" : String.Format(@"={0}+{1}",
-                        result1,
-                        result2)));
-
+                result ? "Success" : "Failure");
             System.Diagnostics.Debug.Assert(dictSaves.Count == listSaves.Count,
                 String.Format("Assertion failed (dictSaves.Count={0}, listSaves.Count={1})",
                     dictSaves.Count,
                     listSaves.Count));
+
             return result;
         }
 
